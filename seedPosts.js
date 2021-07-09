17223e2d9af6fa3f37ad0a035c023dec41f560da
Obi Nwokogba <83872952+obi-nwokogba@users.noreply.github.com>
2021-07-09 10:49:16 -0400

    Final push before presentation

--- seedPosts.js
+++ seedPosts.js
@@ -11,35 +11,35 @@
 */
 
 module.exports = [{
-    postid: 'Newfgds of Today',
-    posttitle: 'A link yuto a photo',
-    posttype: 'Imagedfg Link',
-    posturl: 'https://i.imgur.com/Hz7812B.jpg',
-    posttext: 'A linkdfg to a pic',
+    postid: 'The Cutest lil kitty evr',
+    posttitle: 'The Cutest lil Kitty Ever',
+    posttype: 'Image',
+    posturl: 'https://i.imgur.com/F5iJY.jpg',
+    posttext: 'She was colod this morning. meow.',
     userid: '6'
 },
 {
     postid: 'News odfgdff Today',
-    posttitle: 'A link to a psfdghoto',
-    posttype: 'Imagedf Link',
-    posturl: 'https://i.imgur.com/Hz7812B.jpg',
-    posttext: 'A link fdsfto a pic',
+    posttitle: 'Truth about Starbucks',
+    posttype: 'Image',
+    posturl: 'https://i.imgur.com/7WoKS0S.jpg',
+    posttext: 'ever wondering why you havent woken up?',
     userid: '6'
 },
 {
     postid: 'News of Toddsay',
-    posttitle: 'A dflink to a photo',
-    posttype: 'Image gLink',
-    posturl: 'https://i.imgur.com/Hz7812B.jpg',
-    posttext: 'A lindfsgfk to a pic',
+    posttitle: 'The Altoids metal container....?',
+    posttype: 'Image',
+    posturl: 'https://i.imgur.com/HNvoYzb.jpg',
+    posttext: 'Just wow',
     userid: '6'
 },
 {
     postid: 'News gsof Today',
-    posttitle: 'A linksdgs to a photo',
-    posttype: 'Image gdfLink',
-    posturl: 'https://i.imgur.com/Hz7812B.jpg',
-    posttext: 'A link tcxvo a pic',
+    posttitle: 'Haiti president assasinated...so sad',
+    posttype: 'Text',
+    posturl: '',
+    posttext: 'Haiti is a country under siege as the manhunt for the killers of President Jovenel Moïse ramps up. More than a dozen suspects have been arrested in the last 24 hours, and at least seven others have been killed. Two of those arrested are a U.S. citizens from Florida, James Solages and Joseph Vincent.',
     userid: '346'
 },
 {
