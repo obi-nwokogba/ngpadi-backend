57272cc16f45c024c162b831a9ec666c8ac74321
Obi Nwokogba <83872952+obi-nwokogba@users.noreply.github.com>
2021-07-09 06:14:40 -0400

    Posts Model Added

--- seedPosts.js
+++ seedPosts.js
@@ -1,109 +1,69 @@
 /*
 
-displayname: String,
-    email: String,
-    location: String,
-    status: String,
-    profilepicurl: String,
-    timejoined: String
-
+  postid: String,
+  posttitle: String,
+  posttype: String,
+  posturl: String,
+  posttext: String,
+  userid: String,
+  timecreated: { type: Date, default: Date.now }
 
 */
 
 module.exports = [{
-    displayname: 'Obi Nwokogba',
-    email: 'obi.nwokogba@gmail.com',
-    location: 'Boston, MA',
-    status: 'Working on project 3',
-    profilepicurl: 'https://i.imgur.com/Hz7812B.jpg',
-    timejoined: '6'
+    postid: 'Newfgds of Today',
+    posttitle: 'A link yuto a photo',
+    posttype: 'Imagedfg Link',
+    posturl: 'https://i.imgur.com/Hz7812B.jpg',
+    posttext: 'A linkdfg to a pic',
+    userid: '6'
 },
 {
-    displayname: 'Obi Nwokogba',
-    email: 'obi.nwokogba@gmail.com',
-    location: 'Boston, MA',
-    status: 'Working on project 3',
-    profilepicurl: 'https://i.imgur.com/Hz7812B.jpg',
-    timejoined: '6'
+    postid: 'News odfgdff Today',
+    posttitle: 'A link to a psfdghoto',
+    posttype: 'Imagedf Link',
+    posturl: 'https://i.imgur.com/Hz7812B.jpg',
+    posttext: 'A link fdsfto a pic',
+    userid: '6'
 },
 {
-    displayname: 'Sahid Kapoor',
-    email: 'obi.nwokogba@gmail.com',
-    location: 'Boston, MA',
-    status: 'Working on project 3',
-    profilepicurl: 'https://i.imgur.com/Hz7812B.jpg',
-    timejoined: '6'
+    postid: 'News of Toddsay',
+    posttitle: 'A dflink to a photo',
+    posttype: 'Image gLink',
+    posturl: 'https://i.imgur.com/Hz7812B.jpg',
+    posttext: 'A lindfsgfk to a pic',
+    userid: '6'
 },
 {
-    displayname: 'Obi Nwokogba',
-    email: 'obi.nwokogba@gmail.com',
-    location: 'Boston, MA',
-    status: 'Working on project 3',
-    profilepicurl: 'https://i.imgur.com/Hz7812B.jpg',
-    timejoined: '6'
+    postid: 'News gsof Today',
+    posttitle: 'A linksdgs to a photo',
+    posttype: 'Image gdfLink',
+    posturl: 'https://i.imgur.com/Hz7812B.jpg',
+    posttext: 'A link tcxvo a pic',
+    userid: '346'
 },
 {
-    displayname: 'Obi Nwokogba',
-    email: 'obi.nwokogba@gmail.com',
-    location: 'Boston, MA',
-    status: 'Working on project 3',
-    profilepicurl: 'https://i.imgur.com/Hz7812B.jpg',
-    timejoined: '6'
+    postid: 'News odff Today',
+    posttitle: 'A lidfsnk to a photo',
+    posttype: 'Image Link',
+    posturl: 'https://i.imgur.com/Hz7812B.jpg',
+    posttext: 'A link to a pic',
+    userid: '6'
 },
 {
-    displayname: 'Enrico Fermi',
-    email: 'obi.nwokogba@gmail.com',
-    location: 'Boston, MA',
-    status: 'Working on project 3',
-    profilepicurl: 'https://i.imgur.com/Hz7812B.jpg',
-    timejoined: '6'
+    postid: 'News of Today 33',
+    posttitle: 'A lingdk to a photo',
+    posttype: 'Imagcve Link',
+    posturl: 'https://i.imgur.com/Hz7812B.jpg',
+    posttext: 'A link txco a pic',
+    userid: '6'
 },
 {
-    displayname: 'Erich Fromm',
-    email: 'obi.nwokogba@gmail.com',
-    location: 'Boston, MA',
-    status: 'Working on project 3',
-    profilepicurl: 'https://i.imgur.com/Hz7812B.jpg',
-    timejoined: '6'
-},
-{
-    displayname: 'Truman Capote',
-    email: 'obi.nwokogba@gmail.com',
-    location: 'Boston, MA',
-    status: 'Working on project 3',
-    profilepicurl: 'https://i.imgur.com/Hz7812B.jpg',
-    timejoined: '6'
-},
-{
-    displayname: 'Alexandr Solzhenitsyn',
-    email: 'obi.nwokogba@gmail.com',
-    location: 'Boston, MA',
-    status: 'Working on project 3',
-    profilepicurl: 'https://i.imgur.com/Hz7812B.jpg',
-    timejoined: '6'
-},
-{
-    displayname: 'Walter Scott',
-    email: 'obi.nwokogba@gmail.com',
-    location: 'Boston, MA',
-    status: 'Working on project 3',
-    profilepicurl: 'https://i.imgur.com/Hz7812B.jpg',
-    timejoined: '6'
-},
-{
-    displayname: 'Matt Slim',
-    email: 'mat@slim.com',
-    location: 'Raleigh, NC',
-    status: 'Working on project 3',
-    profilepicurl: 'https://i.imgur.com/Hz7812B.jpg',
-    timejoined: '6'
-},
-{
-    displayname: 'Peter Wesley',
-    email: 'obi.nwokogba@gmail.com',
-    location: 'Newton, MA',
-    status: 'Working on project 3',
-    profilepicurl: 'https://i.imgur.com/Hz7812B.jpg',
-    timejoined: '6'
-},
+    postid: 'News of Today33',
+    posttitle: 'A link to a photo',
+    posttype: 'Image Link',
+    posturl: 'https://i.imgur.com/Hz7812B.jpg',
+    posttext: 'A link to a pic',
+    userid: '6'
+}
 ];
--- server.js
+++ server.js
@@ -21,36 +21,39 @@ const morgan = require("morgan");
 //====================================
 // Establish Connection
 mongoose.connect(DATABASE_URL, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
 });
 
 // Connection Events
 mongoose.connection
-    .on("open", () => console.log("You are connected to MongoDB"))
-    .on("close", () => console.log("You are disconnected from MongoDB"))
-    .on("error", (error) => console.log(error));
+  .on("open", () => console.log("You are connected to MongoDB"))
+  .on("close", () => console.log("You are disconnected from MongoDB"))
+  .on("error", (error) => console.log(error));
 
 //====================================
 // MODELS
 //====================================
 const UserSchema = new mongoose.Schema({
-    displayname: String,
-    email: String,
-    location: String,
-    status: String,
-    profilepicurl: String,
-    timejoined: String
+  displayname: String,
+  email: String,
+  location: String,
+  status: String,
+  profilepicurl: String,
+  timejoined: { type: Date, default: Date.now }
 });
 const User = mongoose.model("User", UserSchema);
 
+
+
 const PostSchema = new mongoose.Schema({
-    displayname: String,
-    email: String,
-    location: String,
-    status: String,
-    profilepicurl: String,
-    timejoined: String
+  postid: String,
+  posttitle: String,
+  posttype: String,
+  posturl: String,
+  posttext: String,
+  userid: String
+  // timecreated: { type: Date, default: Date.now }
 });
 const Post = mongoose.model("Post", PostSchema);
 
@@ -61,83 +64,126 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
-
+//====================================
 // ROUTES
-
 //====================================
 // Seed data with first set of users
 //====================================
 const seedData = require('./seedData.js');
 
 app.get('/seed', (req, res) => {
-    User.deleteMany({}, (error, allUsers) => {})
-    User.create(seedData, (error, data) => {
-        //res.redirect('/users')
-        res.send("Seeding Successful! Users added to Mongo DB!")
-    });
+  User.deleteMany({}, (error, allUsers) => { })
+  User.create(seedData, (error, data) => {
+    //res.redirect('/users')
+    res.send("Seeding Successful! Users added to Mongo DB!")
+  });
+});
+
+//====================================
+// Seed data with first set of users
+//====================================
+const seedData2 = require('./seedPosts.js');
+
+app.get('/seed2', (req, res) => {
+  Post.deleteMany({}, (error, allPosts) => { })
+  Post.create(seedData2, (error, data) => {
+    res.send("Seeding Successful! Posts added!")
+  });
 });
 
 //==============================
 // Home page route
 //==============================
 app.get("/", (req, res) => {
-    res.send("Welcome to NGpadi!");
+  res.send("Welcome to NGpadi!");
 });
 
 //==============================
 // USER INDEX ROUTE
 //==============================
 app.get("/user", async (req, res) => {
-    try {
-        // Return all in User
-        res.json(await User.find({}));
-    } catch (error) {
-        //send error
-        res.status(400).json(error);
-    }
+  try {
+    // Return all in User
+    res.json(await User.find({}));
+  } catch (error) {
+    //send error
+    res.status(400).json(error);
+  }
 });
 
 //==============================
 // USER CREATE ROUTE
 //==============================
 app.post("/user", async (req, res) => {
-    try {
-        let user = await User.create(req.body);
-        res.json(user);
-    } catch (error) {
-        //send error
-        res.send("THERE WAS AN ERROR!");
-        //res.status(400).json(error);
-    }
+  try {
+    let user = await User.create(req.body);
+    res.json(user);
+  } catch (error) {
+    //send error
+    res.send("THERE WAS AN ERROR!");
+    //res.status(400).json(error);
+  }
+});
+
+
+
+
+
+
+
+
+//==============================
+// POST INDEX ROUTE
+//==============================
+app.get("/post", async (req, res) => {
+  try {
+    // Return all in Post
+    res.json(await Post.find({}));
+  } catch (error) {
+    //send error
+    res.status(400).json(error);
+  }
+});
+
+//==============================
+// POST CREATE ROUTE
+//==============================
+app.post("/post", async (req, res) => {
+  try {
+    let post = await Post.create(req.body);
+    res.json(post);
+  } catch (error) {
+    res.send("THERE WAS AN ERROR!");
+  }
 });
 
 //==============================
 // DELETE ROUTE FOR USERS
 //==============================
 app.delete("/user/:id", async (req, res) => {
-    try {
-      // send all people
-      res.json(await User.findByIdAndRemove(req.params.id));
-    } catch (error) {
-      //send error
-      res.status(400).json(error);
-    }
-  });
-  
-  //==============================
-  // UPDATE ROUTE FOR USERS
-  //==============================
-  app.put("/user/:id", async (req, res) => {
-    try {
-      // send all people
-      res.json(
-        await User.findByIdAndUpdate(req.params.id, req.body, { new: true })
-      );
-    } catch (error) {
-      //send error
-      res.status(400).json(error);
-    }
-  });
+  try {
+    // send all people
+    res.json(await User.findByIdAndRemove(req.params.id));
+  } catch (error) {
+    //send error
+    res.status(400).json(error);
+  }
+});
+
+//==============================
+// UPDATE ROUTE FOR USERS
+//==============================
+app.put("/user/:id", async (req, res) => {
+  try {
+    // send all people
+    res.json(
+      await User.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    );
+  } catch (error) {
+    //send error
+    res.status(400).json(error);
+  }
+});
 
 //==============================
 // LISTENER
